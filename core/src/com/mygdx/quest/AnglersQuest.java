dc9a1efa23f12e0f43865073919bee583e5dc424
theJeddi <107645370+jbsalvador06@users.noreply.github.com>
2024-04-18 17:12:56 +0800

    init commit

--- core/src/com/mygdx/quest/AnglersQuest.java
+++ core/src/com/mygdx/quest/AnglersQuest.java
@@ -3,7 +3,7 @@ package com.mygdx.quest;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.quest.screens.MainMenuScreen;
--- core/src/com/mygdx/quest/screens/GameScreen.java
+++ core/src/com/mygdx/quest/screens/GameScreen.java
@@ -21,7 +21,6 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.quest.AnglersQuest;
 import com.mygdx.quest.entities.Player;
